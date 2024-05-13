4ced662b46f80168d9652953c47929758f15a45f
Matthew J. Milner <matterhorn103@proton.me>
2024-05-13 13:00:23 +0200

    Also search for quanstants.toml in user config directory

--- quanstants/config.py
+++ quanstants/config.py
@@ -2,13 +2,30 @@ import __main__
 from pathlib import Path
 import tomllib
 
-# Available options, their default values, and their respective docstrings, are contained in config.toml
+from platformdirs import user_config_dir
 
+# Available options, their default values, and their respective docstrings, are
+# contained in quanstants/config.toml
 
 class QuanstantsConfig:
     def __init__(self):
         # Open default options from file
-        with open(Path(__file__).with_name("config.toml"), "rb") as f:
+        self.load_defaults(Path(__file__).with_name("config.toml"))
+
+    def __setattr__(self, name, value):
+        if hasattr(self, f"_{name}"):
+            # Setting some options needs to trigger changes to other options too
+            if name == "PRETTYPRINT":
+                self._UNICODE_SUPERSCRIPTS = value
+                self._GROUP_DIGITS = value
+                self._PRETTYPRINT = value
+            else:
+                super().__setattr__(f"_{name}", value)
+        else:
+            super().__setattr__(name, value)
+    
+    def load_defaults(self, defaults_file):
+        with open(defaults_file, "rb") as f:
             defaults = tomllib.load(f)
         # Initiate options dynamically
         # Tables within config are just for organization, ignore them
@@ -26,18 +43,6 @@ class QuanstantsConfig:
                         doc=value["doc"],
                     ),
                 )
-
-    def __setattr__(self, name, value):
-        if hasattr(self, f"_{name}"):
-            # Setting some options needs to trigger changes to other options too
-            if name == "PRETTYPRINT":
-                self._UNICODE_SUPERSCRIPTS = value
-                self._GROUP_DIGITS = value
-                self._PRETTYPRINT = value
-            else:
-                super().__setattr__(f"_{name}", value)
-        else:
-            super().__setattr__(name, value)
 
     def read_config(self, file):
         """Read configuration from a quanstants TOML file.
@@ -66,30 +71,34 @@ class QuanstantsConfig:
 
         Currently, the following locations are checked in the given order:
 
-        1. The directory of the main script, from `__main__.__file__`
-        2. The current working directory, from `pathlib.Path.cwd()`
+        1. The current working directory, from `pathlib.Path.cwd()`
+        2. All parent directories of the above
+        3. The user's config directory at ~/.config/quanstants/quanstants.toml on macOS
+        and Linux (or on Linux, $XDG_CONFIG_HOME/quanstants/quanstants.toml),
+        or %USERPROFILE%\AppData\Roaming\quanstants\quanstants.toml on Windows.
 
         Any options specified in the first file found will override the defaults.
         Other files with lower priority in the above list will be completely ignored.
         """
         paths_to_check = []
-        try:
-            paths_to_check.append(Path(__main__.__file__).parent)
-        except AttributeError:
-            pass
+        # Check in current working directory (in case different to above) and in parents
         paths_to_check.append(Path.cwd())
+        paths_to_check.extend(Path.cwd().parents)
+        # Check in user's config directory last
+        paths_to_check.append(user_config_dir("quanstants", roaming=True))
         while (toml_path := None) is None:
             for path in paths_to_check:
                 if (path / "quanstants.toml").exists():
                     toml_path = path / "quanstants.toml"
                 else:
                     continue
+            # Stop once all paths are checked but no quanstants.toml has been found
             break
         if toml_path:
             self.read_config(toml_path)
 
 
 # Create instance of config class that will be passed around to other modules
-# The user should interact with the instance (which is added to the main namespace in __init__.py),
-# not with the class itself
+# The user should interact with the instance (which is added to the main namespace in
+# __init__.py), not with the class itself
 quanfig = QuanstantsConfig()

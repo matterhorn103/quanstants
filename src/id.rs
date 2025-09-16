d84a33c79d02f5bb0f7a9a0cf483726bdce951cc
Matthew Milner <matterhorn103@proton.me>
2025-09-16 05:18:52 +0200

    Make the numerical factor a newtype around u64

--- src/id.rs
+++ src/id.rs
@@ -6,31 +6,45 @@
 // TODO proper hashing
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
-pub struct DecimalFactor {
-    mantissa: u64, // Needs to fit into 48 bits though so restricted to values below 2^48
-    sign: u8,
-    base: u8,
-    exponent: i8,
-}
+pub struct NumericalFactor(u64);
 
-impl From<u64> for DecimalFactor {
-    fn from(value: u64) -> Self {
-        // 8 least significant bits are the exponent
-        let exponent = (value & 0xFF) as i8;
-        // Next 7 bits are the base
-        let base = ((value >> 8) & 0x7F) as u8;
-        let base = if base == 0 { 10 } else { base };
-        // Next bit is the sign
-        let sign = ((value >> 15) & 0x01) as u8;
-        // Most significant 48 bits are the mantissa - 1
-        let mantissa = (value >> 16) + 1;
-        DecimalFactor { mantissa, sign, base, exponent }
+impl NumericalFactor {
+    pub fn exponent(&self) -> i8 {
+        (self.0 & 0xFF) as i8
+    }
+
+    pub fn base(&self) -> u8 {
+        let raw_base = ((self.0 >> 8) & 0x7F) as u8;
+        if raw_base == 0 { 10 } else { raw_base }
+    }
+
+    pub fn sign(&self) -> u8 {
+        ((self.0 >> 15) & 0x01) as u8
+    }
+
+    pub fn mantissa(&self) -> u64 {
+        (self.0 >> 16) + 1
     }
 }
 
+//impl From<u64> for DecimalFactor {
+//    fn from(value: u64) -> Self {
+//        // 8 least significant bits are the exponent
+//        let exponent = (value & 0xFF) as i8;
+//        // Next 7 bits are the base
+//        let base = ((value >> 8) & 0x7F) as u8;
+//        let base = if base == 0 { 10 } else { base };
+//        // Next bit is the sign
+//        let sign = ((value >> 15) & 0x01) as u8;
+//        // Most significant 48 bits are the mantissa - 1
+//        let mantissa = (value >> 16) + 1;
+//        DecimalFactor { mantissa, sign, base, exponent }
+//    }
+//}
+
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
 pub struct UnitId {
-    factor: DecimalFactor,
+    factor: NumericalFactor,
     unit: u64,
 }
 

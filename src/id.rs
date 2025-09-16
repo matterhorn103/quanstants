684f714a0d2e7293d32f3274f428b6775b5d26e3
Matthew Milner <matterhorn103@proton.me>
2025-09-16 05:10:38 +0200

    Add the format for the pre-unit numerical factor

--- src/id.rs
+++ src/id.rs
@@ -1,13 +1,36 @@
 // A UnitId consists of two 64-bit parts:
-//   1. An IEEE 754 decimal64 value indicating a numerical factor
+//   1. A 64-bit decimal in the format |mm|mm|mm|mm|mm|mm|s+b|ee|
 //   2. A 64-bit unit representation
 // All zeroes for the first half of the ID does not indicate a factor of 0 but of 1 
 
 // TODO proper hashing
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct DecimalFactor {
+    mantissa: u64, // Needs to fit into 48 bits though so restricted to values below 2^48
+    sign: u8,
+    base: u8,
+    exponent: i8,
+}
+
+impl From<u64> for DecimalFactor {
+    fn from(value: u64) -> Self {
+        // 8 least significant bits are the exponent
+        let exponent = (value & 0xFF) as i8;
+        // Next 7 bits are the base
+        let base = ((value >> 8) & 0x7F) as u8;
+        let base = if base == 0 { 10 } else { base };
+        // Next bit is the sign
+        let sign = ((value >> 15) & 0x01) as u8;
+        // Most significant 48 bits are the mantissa - 1
+        let mantissa = (value >> 16) + 1;
+        DecimalFactor { mantissa, sign, base, exponent }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
 pub struct UnitId {
-    factor: u64,
+    factor: DecimalFactor,
     unit: u64,
 }
 

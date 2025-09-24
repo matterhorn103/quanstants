d8288190a4e14befb8c054bbf590c25465e91847
Matthew Milner <matterhorn103@proton.me>
2025-09-25 01:55:14 +0200

    Update py tests for changes to hex/bits methods

--- tests/test_frac.py
+++ tests/test_frac.py
@@ -28,98 +28,50 @@ class TestFrac:
         f = Frac(3, 2)
         assert repr(f) == "Frac(3, 2)"
 
-    def test_frac_from_byte0(self):
+    def test_frac_from_bits0(self):
         f = Frac(0, 1)
-        assert f == Frac.from_byte(0x00)
+        assert f == Frac.from_bits(0x00)
 
-    def test_frac_from_byte1(self):
+    def test_frac_from_bits1(self):
         f = Frac(2, 1)
-        assert f == Frac.from_byte(0x12)
+        assert f == Frac.from_bits(0x12)
 
-    def test_frac_from_byte2(self):
+    def test_frac_from_bits2(self):
         f = Frac(1, 2)
-        assert f == Frac.from_byte(0x21)
+        assert f == Frac.from_bits(0x21)
     
-    def test_frac_from_byte3(self):
+    def test_frac_from_bits3(self):
         f = Frac(-1, 1)
-        assert f == Frac.from_byte(0xF1)
+        assert f == Frac.from_bits(0xF1)
 
-    def test_frac_from_byte4(self):
+    def test_frac_from_bits4(self):
         f = Frac(-2, 1)
-        assert f == Frac.from_byte(0xF2)
+        assert f == Frac.from_bits(0xF2)
 
-    def test_frac_from_byte5(self):
+    def test_frac_from_bits5(self):
         f = Frac(-1, 2)
-        assert f == Frac.from_byte(0xE1)
+        assert f == Frac.from_bits(0xE1)
 
-    def test_frac_to_byte0(self):
+    def test_frac_to_bits0(self):
         f = Frac(0, 1)
-        assert f.to_byte() == 0x00
+        assert f.to_bits() == 0x00
 
-    def test_frac_to_byte1(self):
+    def test_frac_to_bits1(self):
         f = Frac(2, 1)
-        assert f.to_byte() == 0x12
+        assert f.to_bits() == 0x12
 
-    def test_frac_to_byte2(self):
+    def test_frac_to_bits2(self):
         f = Frac(1, 2)
-        assert f.to_byte() == 0x21
+        assert f.to_bits() == 0x21
     
-    def test_frac_to_byte3(self):
+    def test_frac_to_bits3(self):
         f = Frac(-1, 1)
-        assert f.to_byte() == 0xF1
+        assert f.to_bits() == 0xF1
 
-    def test_frac_to_byte4(self):
+    def test_frac_to_bits4(self):
         f = Frac(-2, 1)
-        assert f.to_byte() == 0xF2
+        assert f.to_bits() == 0xF2
 
-    def test_frac_to_byte5(self):
+    def test_frac_to_bits5(self):
         f = Frac(-1, 2)
-        assert f.to_byte() == 0xE1
-
-    def test_frac_from_hex0(self):
-        f = Frac(0, 1)
-        assert f == Frac.from_hex("00")
-
-    def test_frac_from_hex1(self):
-        f = Frac(2, 1)
-        assert f == Frac.from_hex("12")
-
-    def test_frac_from_hex2(self):
-        f = Frac(1, 2)
-        assert f == Frac.from_hex("21")
-    
-    def test_frac_from_hex3(self):
-        f = Frac(-1, 1)
-        assert f == Frac.from_hex("F1")
-
-    def test_frac_from_hex4(self):
-        f = Frac(-2, 1)
-        assert f == Frac.from_hex("F2")
-
-    def test_frac_from_hex5(self):
-        f = Frac(-1, 2)
-        assert f == Frac.from_hex("E1")
-
-    def test_frac_to_hex0(self):
-        f = Frac(0, 1)
-        assert f.to_hex() == "00"
-
-    def test_frac_to_hex1(self):
-        f = Frac(2, 1)
-        assert f.to_hex() == "12"
-
-    def test_frac_to_hex2(self):
-        f = Frac(1, 2)
-        assert f.to_hex() == "21"
-    
-    def test_frac_to_hex3(self):
-        f = Frac(-1, 1)
-        assert f.to_hex() == "F1"
-
-    def test_frac_to_hex4(self):
-        f = Frac(-2, 1)
-        assert f.to_hex() == "F2"
-
-    def test_frac_to_hex5(self):
-        f = Frac(-1, 2)
-        assert f.to_hex() == "E1"
+        assert f.to_bits() == 0xE1
--- tests/test_id.py
+++ tests/test_id.py
@@ -4,11 +4,6 @@ class TestUnitId:
     def test_new_second(self):
         u = UnitId(0, 0x1100)
 
-    def test_metre_to_hex(self):
+    def test_metre_to_str(self):
         u = UnitId(0, 0x110000)
-        assert u.to_hex() == "110000"
-    
-    def test_kilogram_from_hex(self):
-        u = UnitId.from_hex("11000000")
-        assert u == UnitId(0, 0x11000000)
-
+        assert str(u) == "UnitId(110000)"
